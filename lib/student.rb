2391bf6ead63cdc74910b976c78f6dc791e46fcd
JeremyKungu <jeremikungu27@gmail.com>
2022-10-25 09:47:01 +0300

    tests passing

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,23 @@
-class Student 
+class Student < User
+    def knowledge=(knowledge)
+        @knowledge = knowledge
+      end
+  
+      def knowledge
+        @knowledge
+      end
+
+      def initialize
+        @knowledge = []
+      end      
+
+    def learn(string)
+        @string = string
+        knowledge << string
+    end
+
+    def knowledge
+        @knowledge
+    end
 
 end
